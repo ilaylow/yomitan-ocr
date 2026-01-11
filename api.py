c97bd1a9886768853c8c6da1dfb36f4f6abc02c4
Chuen Ley Low <chuenleylow@gmail.com>
2026-01-11 15:36:28 +1100

    added in secondary ocr model

--- api.py
+++ api.py
@@ -4,8 +4,20 @@ from huggingface_hub import hf_hub_download
 from pydantic import BaseModel
 import numpy as np
 import cv2
+from fastapi.middleware.cors import CORSMiddleware
+from manga_ocr import MangaOcr
+from PIL import Image
+import io
 
 app = FastAPI(title="OCR API", description="API for detecting text in images")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or ["http://localhost:xxxx"]
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Model paths
 det_path = "/Users/chuenleylow/Downloads/onnx-test/ppocrv5_det.onnx"
@@ -16,8 +28,13 @@ dict_path = hf_hub_download("monkt/paddleocr-onnx", "languages/chinese/dict.txt"
 ocr = RapidOCR(
     det_model_path=det_path,
     rec_model_path=rec_path,
-    rec_keys_path=dict_path
+    rec_keys_path=dict_path,
+    det_db_box_thresh=0.1,  # lower from default 0.5 - more sensitive detection
+    det_db_unclip_ratio=1.8,
 )
+
+# Initialize secondary OCR model
+mocr = MangaOcr()
 
 
 class DetectedWord(BaseModel):
@@ -29,7 +46,7 @@ class OCRResponse(BaseModel):
     words: list[DetectedWord]
 
 
-@app.post("/yomitan/ocr", response_model=OCRResponse)
+@app.post("/yomitan/ocr")
 async def process_image(file: UploadFile = File(...)):
     """
     Process an uploaded image and return detected text with confidence scores.
@@ -43,22 +60,29 @@ async def process_image(file: UploadFile = File(...)):
     nparr = np.frombuffer(contents, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    if img is None:
+    pil_image = Image.open(io.BytesIO(nparr))
+
+    if img is None or pil_image is None:
         raise HTTPException(status_code=400, detail="Could not decode image")
 
+
     result, elapsed = ocr(img)
-    print(f"Detection: {elapsed[0]:.3f}s, Classification: {elapsed[1]:.3f}s, Recognition: {elapsed[2]:.3f}s")
-
-    if result is None:
+    mocr_result = mocr(pil_image)
+    if result is None and mocr_result is None:
         return OCRResponse(words=[])
+    
+    if elapsed:
+        print(f"(Primary Model) Detection: {elapsed[0]:.3f}s, Classification: {elapsed[1]:.3f}s, Recognition: {elapsed[2]:.3f}s")
 
-    words = [
-        DetectedWord(text=res[1], confidence=round(float(res[2]) * 100, 2))
-        for res in result
-    ]
-
-    return OCRResponse(words=words)
-
+    if result is not None:
+        words = [
+            DetectedWord(text=res[1], confidence=round(float(res[2]) * 100, 2))
+            for res in result
+        ]
+        return {"words": words, "type": "primary"}
+    else:
+        words = mocr_result
+        return {"words": [words], "type": "secondary"}
 
 @app.get("/health")
 async def health_check():
